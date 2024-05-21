ce69479c47522ebe988d1ff587242c43eec52b9c
unanp2 <unanp2@gmail.com>
2024-05-21 15:09:29 +0900

    별점 평균 나오게 수정

--- src/main/java/com/capstonedk/Maven/model/response/UserProfileResponse.java
+++ src/main/java/com/capstonedk/Maven/model/response/UserProfileResponse.java
@@ -10,13 +10,11 @@ import java.util.List;
 @Getter
 @Setter
 public class UserProfileResponse {
-    private Long userId;
     private String loginId;
     private String nickname;
     private List<ReviewDTO> reviews;
 
     public UserProfileResponse(User user, List<ReviewDTO> reviews) {
-        this.userId = user.getUserId();
         this.loginId = user.getLoginId();
         this.nickname = user.getNickname();
         this.reviews = reviews;
